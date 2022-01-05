d783cc4528fe9a47d35b7bb769bffb7173922196
Jacky Zhang <jackyyzhang@gmail.com>
2022-01-04 22:52:23 -0500

    Add prompt to update PWA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,12 @@
 import "./src/styles/styles.css";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
