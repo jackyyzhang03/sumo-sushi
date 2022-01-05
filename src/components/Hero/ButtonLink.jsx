cf6462b28f3d3a850de71571992beae3d562de74
Jacky Zhang <jackyyzhang@gmail.com>
2022-01-04 21:29:11 -0500

    Adjust animations

--- src/components/Hero/ButtonLink.jsx
+++ src/components/Hero/ButtonLink.jsx
@@ -14,7 +14,7 @@ const ButtonLink = styled(Link)`
   font-size: 16px;
   vertical-align: middle;
   line-height: 60px;
-  animation: fade 2s ease 1s, slide-up 2s ease 1s;
+  animation: fade 4s ease, slide-up 3s ease;
 
   &:hover {
     outline-width: 2px;
