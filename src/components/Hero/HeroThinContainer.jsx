ffdc66a0eb384b347959ad78e60cd2e8112eb61e
Jacky Zhang <jackyyzhang@gmail.com>
2022-02-03 19:52:55 -0500

    Resize hero images

--- src/components/Hero/HeroThinContainer.jsx
+++ src/components/Hero/HeroThinContainer.jsx
@@ -10,8 +10,12 @@ const HeroThinContainer = styled.div`
 
   img, .gatsby-image-wrapper > div[aria-hidden=true] {
     padding-top: 0 !important;
-    height: 500px !important;
+    height: 450px !important;
     filter: contrast(105%);
+  }
+
+  @media only screen and (max-width: 768px) {
+    height: 280px;
   }
 `;
 
